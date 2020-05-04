fbfe3dd07e62f0ec20e546f71230cdf8e26d9f7d
Aleksandr <lilxtent@gmail.com>
2020-05-04 14:44:49 +0700

    Format style

--- src/file.hpp
+++ src/file.hpp
@@ -2,11 +2,12 @@
 
 #include <fstream>
 
-void make_html (std::string format_html, std::string path = "chess_html.html"){
+void make_html(std::string format_html, std::string path = "chess_html.html")
+{
     std::ofstream file;
     file.open(path, std::ios::out | std::ios::app);
 
-    if (!file.is_open()){
+    if (!file.is_open()) {
         std::cout << "No such file" << std::endl;
         return;
     }
@@ -14,5 +15,4 @@ void make_html (std::string format_html, std::string path = "chess_html.html"){
     file << format_html;
 
     file.close();
-
 }

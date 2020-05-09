92a48b0eff1389b26483bd9b88424f0a3246e7c3
Aleksandr <lilxtent@gmail.com>
2020-05-09 15:00:24 +0700

    Func make_html moved to class File

---
+++ src/File.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <fstream>
+
+class File {
+private:
+public:
+    File()
+    {
+    }
+
+    void
+    make_html(std::string format_html, std::string path = "chess_html.html")
+    {
+        std::ofstream file;
+        file.open(path, std::ios::out | std::ios::app);
+
+        if (!file.is_open()) {
+            std::cout << "No such file" << std::endl;
+            return;
+        }
+
+        file << format_html;
+
+        file.close();
+    }
+};

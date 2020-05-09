36665cf39a39958775e1bfa30819928601e4f23a
Aleksandr <lilxtent@gmail.com>
2020-05-09 14:17:40 +0700

    Global field moved to File class

--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,6 @@
 #include "file.hpp"
 #include "make_html.hpp"
 
-std::string field[9][9] = {{""},
-                           {"",
-                            "<span class=\"white rook\"></span>",
-                            "<span class=\"white knight\"></span>",
-                            "<span class=\"white bishop\"></span>",
-                            "<span class=\"white queen\"></span>",
-                            "<span class=\"white king\"></span>",
-                            "<span class=\"white bishop\"></span>",
-                            "<span class=\"white knight\"></span>",
-                            "<span class=\"white rook\"></span>"},
-                           {"",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>",
-                            "<span class=\"white pawn\"></span>"},
-                           {"", "", "", "", "", "", "", "", ""},
-                           {"", "", "", "", "", "", "", "", ""},
-                           {"", "", "", "", "", "", "", "", ""},
-                           {"", "", "", "", "", "", "", "", ""},
-                           {"",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>",
-                            "<span class=\"black pawn\"></span>"},
-                           {"",
-                            "<span class=\"black rook\"></span>",
-                            "<span class=\"black knight\"></span>",
-                            "<span class=\"black bishop\"></span>",
-                            "<span class=\"black queen\"></span>",
-                            "<span class=\"black king\"></span>",
-                            "<span class=\"black bishop\"></span>",
-                            "<span class=\"black knight\"></span>",
-                            "<span class=\"black rook\"></span>"}};
-
 int main(int argc, char* argv[])
 {
     std::string file_name = "";

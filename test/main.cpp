6ffde424ebf4cd0f428d0a1f29499ca0dd09850a
Aleksandr <lilxtent@gmail.com>
2020-05-10 00:37:29 +0700

    Test for Converter::letters_to_nums

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include "../thirdparty/catch2.hpp"
 
-#include "../src/Converter.hpp"
-#include "../src/Field.hpp"
-#include "../src/File.hpp"
+#include "../src/Converter.cpp"
+#include "../src/Field.cpp"
+#include "../src/File.cpp"
+
+TEST_CASE("Letters converts to nums correctly", "[letter]")
+{
+    char letter;
+    char num;
+
+    Converter convert;
+
+    for (int i = 97; i < 97 + 8; i++) {
+        letter = i;
+        num = letter - 48;
+        convert.letters_to_nums(letter);
+        REQUIRE(letter == num);
+    }
+}

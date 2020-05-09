cfa3b60f223146a25de8f60d5e29f2ed76c77e1c
Aleksandr <lilxtent@gmail.com>
2020-05-10 00:50:00 +0700

    Test for Field::make_html_head & Field::close_html_code

--- test/main.cpp
+++ test/main.cpp
@@ -5,7 +5,8 @@
 #include "../src/Field.cpp"
 #include "../src/File.cpp"
 
-TEST_CASE("Letters converts to nums correctly", "[letter]")
+// Converter::letters_to_nums
+TEST_CASE("Letters converts to nums correctly")
 {
     char letter;
     char num;
@@ -18,4 +19,60 @@ TEST_CASE("Letters converts to nums correctly", "[letter]")
         convert.letters_to_nums(letter);
         REQUIRE(letter == num);
     }
+}
+// Field::make_html_head & Field::close_html_code
+TEST_CASE("Static HTML template generate correctly")
+{
+    Field field;
+
+    REQUIRE(field.make_html_head() == R"(
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>chessviz</title>
+<style type="text/css">
+table.chessboard {
+border: 5px solid #333;
+border-collapse: collapse;
+height: 320px;
+margin: 20px;
+width: 320px;
+}
+table.chessboard caption {
+text-align: left;
+}
+table.chessboard td {
+background-color: #fff;
+font-size: 25px;
+height: 40px;
+text-align: center;
+vertical-align: middle;
+width: 40px;
+}
+table.chessboard tr:nth-child(odd) td:nth-child(even),
+table.chessboard tr:nth-child(even) td:nth-child(odd) {
+background-color: #999;
+}
+
+table.chessboard .white.king:before   { content: "\2654"; }
+table.chessboard .white.queen:before  { content: "\2655"; }
+table.chessboard .white.rook:before   { content: "\2656"; }
+table.chessboard .white.bishop:before { content: "\2657"; }
+table.chessboard .white.knight:before { content: "\2658"; }
+table.chessboard .white.pawn:before   { content: "\2659"; }
+
+table.chessboard .black.king:before   { content: "\265A"; }
+table.chessboard .black.queen:before  { content: "\265B"; }
+table.chessboard .black.rook:before   { content: "\265C"; }
+table.chessboard .black.bishop:before { content: "\265D"; }
+table.chessboard .black.knight:before { content: "\265E"; }
+table.chessboard .black.pawn:before   { content: "\265F"; }
+</style>
+</head>
+)");
+
+    REQUIRE(field.close_html_code() == R"(
+</html>
+    )");
 }
